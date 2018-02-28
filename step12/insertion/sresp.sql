f45a4aab58a1fb9d74039e5c0d9863c838f28372
GOUDA Rayane <rgouda@jems-group.com>
2018-02-28 17:54:01 +0100

    Scripts dinsertion de lignes

--- step12/insertion/sresp.sql
+++ step12/insertion/sresp.sql
@@ -13,7 +13,7 @@ DELIMITER $$
  
  WHILE fin  <= 3600 DO
  SET ROW_ID_V=CONCAT(x,'-4ZJ7T');
- SET NAME_V=CONCAT(x, '-Content Manager');
+ SET NAME_V=CONCAT(x, '-Manager');
  SET  x = x + 1; 
  INSERT INTO s_resp ( ROW_ID,  NAME ) VALUES ( ROW_ID_V , NAME_V );
  SELECT SLEEP(1);
